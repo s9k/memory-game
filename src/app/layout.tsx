79da4cddd22c592380e97c6c185d66b25e85c304
s9k <gc.s9k0@gmail.com>
2025-06-04 19:36:09 +0100

    Integrate @vercel/speed-insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Space_Grotesk } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { PageHeader, PageContainer } from "@/widgets";
 import { TITLE } from "@/constants";
 import "./globals.css";
@@ -25,6 +26,7 @@ export default function RootLayout({
       <body className={`${spaceGrotesk.variable}`}>
         <PageHeader />
         <PageContainer>{children}</PageContainer>
+        <SpeedInsights />
       </body>
     </html>
   );
