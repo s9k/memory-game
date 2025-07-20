5e72a88aef707decc08b5c23cace99031456eb77
s9k <gc.s9k0@gmail.com>
2025-07-20 21:43:18 +0100

    Remove zod deprecations

--- src/api/pexel/model/listReponse.ts
+++ src/api/pexel/model/listReponse.ts
@@ -1,23 +1,23 @@
 import { z } from "zod";
 
 const srcSchema = z.object({
-  original: z.string().url(),
-  large2x: z.string().url(),
-  large: z.string().url(),
-  medium: z.string().url(),
-  small: z.string().url(),
-  portrait: z.string().url(),
-  landscape: z.string().url(),
-  tiny: z.string().url(),
+  original: z.url(),
+  large2x: z.url(),
+  large: z.url(),
+  medium: z.url(),
+  small: z.url(),
+  portrait: z.url(),
+  landscape: z.url(),
+  tiny: z.url(),
 });
 
 const photoSchema = z.object({
   id: z.number(),
   width: z.number(),
   height: z.number(),
-  url: z.string().url(),
+  url: z.url(),
   photographer: z.string(),
-  photographer_url: z.string().url(),
+  photographer_url: z.url(),
   photographer_id: z.number(),
   avg_color: z.string(),
   liked: z.boolean(),
@@ -30,7 +30,7 @@ export const apiPexelListResponseSchema = z.object({
   per_page: z.number(),
   photos: z.array(photoSchema),
   total_results: z.number(),
-  next_page: z.string().url().optional(),
+  next_page: z.url().optional(),
 });
 
 export type ApiPexelListResponse = z.infer<typeof apiPexelListResponseSchema>;
